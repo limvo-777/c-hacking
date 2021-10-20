fb394d4082da56c37ab82a8403e5591c9950eede
limvo-777 <lejpf54@gmail.com>
2021-10-20 09:02:52 +0900

    obfuscation

---
+++ obfuscation/obfuse.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+        //this is simple print test program for floats 
+        float b[] = {1.1431391224375825e+27, 6.6578486920496456e+28, 7.73929309656277983e-19,3.2512161851627752e-9};
+		
+		puts((char*)&b);
+		
+		
+		
+		return 0;
+		
+
+
+
+
+ } 
+
+
+
+
